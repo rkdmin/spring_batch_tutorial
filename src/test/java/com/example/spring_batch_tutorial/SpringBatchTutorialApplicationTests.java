78d4e514a7f114497eea1a874e4cf9495879ff2e
kangmo <22rkdmin@naver.com>
2023-10-28 17:36:52 +0900

    init

---
+++ src/test/java/com/example/spring_batch_tutorial/SpringBatchTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_batch_tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
