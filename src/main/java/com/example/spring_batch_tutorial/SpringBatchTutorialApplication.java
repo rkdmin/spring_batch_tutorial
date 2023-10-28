eb1ea8d59414bbcc85f7dcaca60f45618baea7b8
kangmo <22rkdmin@naver.com>
2023-10-28 19:46:46 +0900

    hello world

--- src/main/java/com/example/spring_batch_tutorial/SpringBatchTutorialApplication.java
+++ src/main/java/com/example/spring_batch_tutorial/SpringBatchTutorialApplication.java
@@ -1,8 +1,10 @@
 package com.example.spring_batch_tutorial;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableBatchProcessing// 배치 작동
 @SpringBootApplication
 public class SpringBatchTutorialApplication {
 
