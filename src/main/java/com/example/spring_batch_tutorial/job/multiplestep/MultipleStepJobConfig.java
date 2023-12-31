e05378540a52b946d2d00ba01a0197eee5915a61
kangmo <22rkdmin@naver.com>
2024-01-01 02:15:28 +0900

    multipleStep1

---
+++ src/main/java/com/example/spring_batch_tutorial/job/multiplestep/MultipleStepJobConfig.java
@@ -0,0 +1,88 @@
+package com.example.spring_batch_tutorial.job.multiplestep;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * desc: 다중 step을 사용하기 및 step to step 데이터 전달
+ * run param: --job.name=multipleStepJob
+ */
+@Configuration
+@RequiredArgsConstructor
+public class MultipleStepJobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job multipleStepJob(Step multipleStep1, Step multipleStep2, Step multipleStep3) {
+        return jobBuilderFactory.get("multipleStepJob")
+                .incrementer(new RunIdIncrementer())
+                .start(multipleStep1)// 여러 개의 step 구성
+                .next(multipleStep2)
+                .next(multipleStep3)
+                .build();
+    }
+
+    @JobScope
+    @Bean
+    public Step multipleStep1() {
+        return stepBuilderFactory.get("multipleStep1")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step1");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @JobScope
+    @Bean
+    public Step multipleStep2() {
+        return stepBuilderFactory.get("multipleStep2")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step2");
+
+                    // 다음 스텝에 데이터를 보내기 위해 ExecutionContext 사용
+                    ExecutionContext executionContext = chunkContext
+                            .getStepContext()
+                            .getStepExecution()
+                            .getJobExecution()
+                            .getExecutionContext();
+
+                    executionContext.put("someKey", "hello!!");// key-value 형식으로 저장
+
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @JobScope
+    @Bean
+    public Step multipleStep3() {
+        return stepBuilderFactory.get("multipleStep3")
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("step3");
+
+                    ExecutionContext executionContext = chunkContext
+                            .getStepContext()
+                            .getStepExecution()
+                            .getJobExecution()
+                            .getExecutionContext();
+
+                    System.out.println(executionContext.get("someKey"));// step2의 데이터 출력
+
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+}
